93b54b805afbe62a318bfff71f5b3bae2918564e
Kirill Mironov <22131753+KirillMironov@users.noreply.github.com>
2022-08-26 12:16:59 +0300

    Status

--- lib/domain/status.dart
+++ lib/domain/status.dart
@@ -4,23 +4,17 @@ enum Status {
   skipped,
   inProgress;
 
-  static const Map<Status, String> toMap = {
-    Status.success: 'success',
-    Status.failure: 'failure',
-    Status.skipped: 'skipped',
-    Status.inProgress: 'in progress',
-  };
+  name() => {
+        Status.success: 'success',
+        Status.failure: 'failure',
+        Status.skipped: 'skipped',
+        Status.inProgress: 'in progress',
+      }[this];
 
-  static const Map<String, Status> fromMap = {
-    'success': Status.success,
-    'failure': Status.failure,
-    'skipped': Status.skipped,
-    'in progress': Status.inProgress,
-  };
-
-  name() => toMap[this];
-
-  factory Status.fromString(String value) {
-    return fromMap[value] ?? (throw ArgumentError('Unknown status: $value'));
-  }
+  factory Status.fromString(String str) => {
+        'success': Status.success,
+        'failure': Status.failure,
+        'skipped': Status.skipped,
+        'in progress': Status.inProgress,
+      }[str] ??= (throw ArgumentError('Unknown status: $str'));
 }
